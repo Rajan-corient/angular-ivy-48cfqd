f3ded0cfec016b0e4a3297b3f39c44c8c94fbfdc
Rajan-corient <rajan.gupta@corientbs.com>
2022-03-23 21:53:00 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, VERSION } from '@angular/core';
+import { Person, Payment } from '../meta-data';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  personMetaData = Person;
+  paymentMetaData = Payment;
+
+  ngOnInit() {
+    console.log('personMetaData', this.personMetaData);
+    console.log('paymentMetaData', this.paymentMetaData);
+  }
+}
